21a9f82f369d495b8cebb71e5a8cc74ccca8be5e
vahan <vahan@vahan-system>
2016-11-03 18:31:33 +0400

    Kalc by kafka

---
+++ src/main/java/co/oddeye/calculator/CalcByKafkaTask.java
@@ -0,0 +1,271 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.oddeye.calculator;
+
+/**
+ *
+ * @author vahan
+ */
+import co.oddeye.core.MetriccheckRule;
+import co.oddeye.core.OddeeyMetricMeta;
+import co.oddeye.core.OddeeyMetricMetaList;
+import com.google.gson.Gson;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import net.opentsdb.core.TSDB;
+import org.hbase.async.HBaseClient;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.stumbleupon.async.Deferred;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import kafka.message.MessageAndMetadata;
+import net.opentsdb.core.DataPoints;
+import org.hbase.async.PutRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalcByKafkaTask implements Runnable {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(CalcByKafkaTask.class);
+    private final KafkaStream m_stream;
+    private final int m_threadNumber;
+
+    private final HBaseClient m_client;
+    private final TSDB m_tsdb;
+    private final JsonParser parser;
+    private JsonArray jsonResult;    
+    private MetriccheckRule Rule;    
+    private final HashMap<Object, Object> tags = new HashMap<>();
+    private long metrictime;
+    private final Calendar StartCalendarObj = new Calendar.Builder().build();
+    private final Calendar EndCalendarObj = new Calendar.Builder().build();
+    private long starttime;
+    private long endtime;
+    private boolean needsave;
+
+    public CalcByKafkaTask(KafkaStream a_stream, int a_threadNumber, TSDB tsdb, HBaseClient client) {
+        m_threadNumber = a_threadNumber;
+        m_stream = a_stream;
+        m_client = client;
+        m_tsdb = tsdb;
+        parser = new JsonParser();
+    }
+
+    @Override
+    public void run() {
+        final Calendar CalendarObj = Calendar.getInstance();
+        final Calendar CalendarObjRules = Calendar.getInstance();
+
+        NumberFormat formatter = new DecimalFormat("#0.0000");
+        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
+        JsonElement Metric;
+        OddeeyMetricMeta mtrsc;
+        OddeeyMetricMetaList mtrscList;
+        byte[] metatable = "test_oddeye-meta".getBytes();
+        try {
+            mtrscList = new OddeeyMetricMetaList(m_tsdb, metatable);
+        } catch (Exception ex) {
+            mtrscList = new OddeeyMetricMetaList();
+        }
+
+        byte[] key;
+        byte[] family = "d".getBytes();
+        byte[] qualifier;
+        byte[] value;
+        Gson gson = new Gson();
+        final Calendar RuleStart = Calendar.getInstance();
+        final Calendar RuleEnd = Calendar.getInstance();
+
+        RuleEnd.set(CalendarObj.get(Calendar.YEAR), CalendarObj.get(Calendar.MONTH), CalendarObj.get(Calendar.DAY_OF_MONTH), CalendarObj.get(Calendar.HOUR_OF_DAY), 0);
+        RuleStart.set(CalendarObj.get(Calendar.YEAR), CalendarObj.get(Calendar.MONTH), CalendarObj.get(Calendar.DAY_OF_MONTH) - 1, CalendarObj.get(Calendar.HOUR_OF_DAY), 0);
+
+        while (it.hasNext()) {            
+            final MessageAndMetadata<byte[], byte[]> message = it.next();
+            final String msg = new String(message.message());            
+
+//            System.out.println("Offset:" + message.offset());
+//            final String msg = "[{\"timestamp\":1475564807,\"metric\":\"mem_total\",\"value\":\"4085243904\",\"tags\":{\"cluster\":\"MouseflowEU\",\"host\":\"vpneu1.mouseflow.eu\",\"type\":\"system\",\"alert_level\":0,\"group\":\"vpn\",\"UUID\":\"da476957-6bfc-4fa0-acd0-fd0fde098fd4\"}}]";
+            try {
+                jsonResult = (JsonArray) this.parser.parse(msg);
+                if (this.jsonResult != null) {
+                    try {
+                        if (this.jsonResult.size() > 0) {
+                            LOGGER.info("Ready count: " + this.jsonResult.size());
+                            CalendarObjRules.setTime(new Date());
+                            CalendarObjRules.add(Calendar.HOUR, -1);
+
+                            for (int i = 0; i < this.jsonResult.size(); i++) {
+                                Metric = this.jsonResult.get(i);
+
+                                if (Metric.getAsJsonObject().get("tags") != null) {
+                                    if (Metric.getAsJsonObject().get("tags").getAsJsonObject().get("host") == null) {
+                                        LOGGER.warn("host not exist in input " + msg);
+                                    }
+                                    if (Metric.getAsJsonObject().get("tags").getAsJsonObject().get("type") == null) {
+                                        LOGGER.warn("type not exist in input " + msg);
+                                    }
+                                    if (Metric.getAsJsonObject().get("tags").getAsJsonObject().get("alert_level") == null) {
+                                        LOGGER.warn("alert_level not exist in input " + msg);
+                                    }
+                                    if (Metric.getAsJsonObject().get("tags").getAsJsonObject().get("group") == null) {
+                                        LOGGER.warn("group not exist in input " + msg);
+                                    }
+                                    if (Metric.getAsJsonObject().get("timestamp") == null) {
+                                        LOGGER.warn("timestamp not exist in input " + msg);
+                                    }
+
+                                    if (Metric.getAsJsonObject().get("metric") == null) {
+                                        LOGGER.warn("metric not exist in input " + msg);
+                                    }
+                                    if (Metric.getAsJsonObject().get("value") == null) {
+                                        LOGGER.warn("value not exist in input " + msg);
+                                    }
+                                } else {
+                                    LOGGER.warn("tags not exist in input " + msg);
+                                }
+
+                                CalendarObj.setTimeInMillis(Metric.getAsJsonObject().get("timestamp").getAsLong() * 1000);
+                                mtrsc = new OddeeyMetricMeta(Metric, m_tsdb);
+//                                if (!mtrsc.getName().equals("cpu_user")) {
+//                                    continue;
+//                                }
+//                                if (!mtrsc.getTags().get("host").getValue().contains("cassa005")) {
+//                                    continue;
+//                                }
+
+//                                metric_index = mtrscList.containsKey(mtrsc.hashCode());
+                                if (mtrscList.containsKey(mtrsc.hashCode())) {
+                                    mtrsc = mtrscList.get(mtrsc.hashCode());
+                                }
+
+                                metrictime = Metric.getAsJsonObject().get("timestamp").getAsLong() * 1000;
+
+                                CalendarObjRules.setTimeInMillis(metrictime);
+                                CalendarObjRules.add(Calendar.HOUR, 1);
+
+                                LOGGER.info(CalendarObj.getTime() + "-" + Metric.getAsJsonObject().get("metric").getAsString() + " " + Metric.getAsJsonObject().get("tags").getAsJsonObject().get("host").getAsString());
+                                needsave = false;
+                                final ArrayList<Deferred<DataPoints[]>> deferreds = new ArrayList<>();
+                                for (int j = 0; j < 7; j++) {
+                                    CalendarObjRules.add(Calendar.DATE, -1);
+                                    try {
+                                        Rule = mtrsc.getRule(CalendarObjRules, metatable, m_client);
+                                    } catch (Exception ex) {
+                                        LOGGER.warn("Rule exeption: " + CalendarObjRules.getTime() + "-" + mtrsc.getName() + " " + mtrsc.getTags().get("host").getValue());
+                                        LOGGER.warn("RuleExeption: " + stackTrace(ex));
+                                    }
+                                    if (Rule == null) {
+                                        LOGGER.warn("Rule is NUll: " + CalendarObjRules.getTime() + "-" + mtrsc.getName() + " " + mtrsc.getTags().get("host").getValue());
+                                        continue;
+                                    }
+
+                                    if ((!Rule.isIsValidRule()) && (!Rule.isHasNotData())) {
+                                        needsave = true;
+
+                                        StartCalendarObj.setTime(CalendarObjRules.getTime());
+                                        EndCalendarObj.setTime(CalendarObjRules.getTime());
+                                        EndCalendarObj.add(Calendar.HOUR, 1);
+                                        EndCalendarObj.set(Calendar.MILLISECOND, 0);
+                                        EndCalendarObj.set(Calendar.SECOND, 0);
+                                        EndCalendarObj.set(Calendar.MINUTE, 0);
+                                        EndCalendarObj.add(Calendar.MILLISECOND, -1);
+                                        StartCalendarObj.set(Calendar.MILLISECOND, 0);
+                                        StartCalendarObj.set(Calendar.SECOND, 0);
+                                        StartCalendarObj.set(Calendar.MINUTE, 0);
+                                        ArrayList<Deferred<DataPoints[]>> rule_deferreds = mtrsc.CalculateRulesApachMath(StartCalendarObj.getTimeInMillis(), EndCalendarObj.getTimeInMillis(), m_tsdb);
+                                        deferreds.addAll(rule_deferreds);
+
+                                    } 
+
+                                }
+                                if (deferreds.size() > 0) {
+                                    starttime = System.currentTimeMillis();
+                                    Deferred.groupInOrder(deferreds).joinUninterruptibly();
+                                    endtime = System.currentTimeMillis() - starttime;
+                                    LOGGER.warn("Rule joinUninterruptibly " + CalendarObjRules.getTime() + " to 3 houre time: " + endtime + " Name:" + mtrsc.getName() + " host" + mtrsc.getTags().get("host").getValue());
+                                }
+                                else {
+                                        LOGGER.warn("All Rule is Exist: " + CalendarObjRules.getTime() + "-" + mtrsc.getName() + " " + mtrsc.getTags().get("host").getValue());
+                                        //+ "-" + mtrsc.getName() + " " + mtrsc.getTags().get("host").getValue()
+                                    }
+                                if (needsave) {
+                                    key = mtrsc.getKey();
+                                    byte[][] qualifiers;
+                                    byte[][] values;
+                                    ConcurrentMap<String, MetriccheckRule> rulesmap = mtrsc.getRulesMap();
+                                    qualifiers = new byte[rulesmap.size()][];
+                                    values = new byte[rulesmap.size()][];
+                                    int index = 0;
+                                    for (Map.Entry<String, MetriccheckRule> rule : rulesmap.entrySet()) {
+                                        qualifiers[index] = rule.getValue().getKey();
+                                        values[index] = rule.getValue().getValues();
+                                        index++;
+                                    }
+
+                                    if (qualifiers.length > 0) {
+                                        PutRequest putvalue = new PutRequest(metatable, key, family, qualifiers, values);
+                                        m_client.put(putvalue);
+                                    } else {
+                                        PutRequest putvalue = new PutRequest(metatable, key, family, "n".getBytes(), key);
+                                        m_client.put(putvalue);
+                                    }
+                                }
+                                mtrscList.set(mtrsc);
+                            }
+                        }
+                    } catch (JsonSyntaxException ex) {
+                        LOGGER.error("JsonSyntaxException: " + stackTrace(ex));
+//                this.collector.fail(input);
+                    } catch (NumberFormatException ex) {
+                        LOGGER.error("NumberFormatException: " + stackTrace(ex));
+//                this.collector.fail(input);
+                    } catch (Exception ex) {
+                        LOGGER.error("Exception: " + stackTrace(ex));
+//                        System.exit(0);
+//                this.collector.fail(input);
+                    }
+                    this.jsonResult = null;
+                }
+
+            } catch (Exception e) {
+                LOGGER.error("Check Error:" + stackTrace(e));
+
+                break;
+            }
+        }
+        System.out.println("Shutting down Thread: " + m_threadNumber);
+        try {
+            m_tsdb.shutdown().joinUninterruptibly();
+        } catch (Exception ex) {
+            System.out.println("m_tsdb.shutdown: " + ex);
+
+        }
+    }
+
+    private String stackTrace(Exception cause) {
+        if (cause == null) {
+            return "";
+        }
+        StringWriter sw = new StringWriter(1024);
+        final PrintWriter pw = new PrintWriter(sw);
+        cause.printStackTrace(pw);
+        pw.flush();
+        return sw.toString();
+    }
+}
--- src/main/java/co/oddeye/calculator/MainClass.java
+++ src/main/java/co/oddeye/calculator/MainClass.java
@@ -5,74 +5,106 @@
  */
 package co.oddeye.calculator;
 
-import co.oddeye.core.MetriccheckRule;
-import co.oddeye.core.OddeeyMetricMeta;
-import co.oddeye.core.OddeeyMetricMetaList;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
+
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
+import java.util.Properties;
+import java.util.Calendar;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
 import net.opentsdb.core.TSDB;
-import net.opentsdb.utils.DateTime;
+import net.opentsdb.utils.Config;
 import org.apache.log4j.PropertyConfigurator;
-import org.hbase.async.BinaryComparator;
-import org.hbase.async.CompareFilter;
-import org.hbase.async.GetRequest;
-import org.hbase.async.KeyValue;
-import org.hbase.async.PutRequest;
-import org.hbase.async.QualifierFilter;
-import org.hbase.async.ScanFilter;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.yaml.snakeyaml.Yaml;
 
-//import org.apache.samoa.core.Processor;
-//import org.apache.samoa.topology.Stream;
-//import org.apache.samoa.topology.TopologyBuilder;
 /**
  *
  * @author vahan
  */
 public class MainClass {
 
+    private final ConsumerConnector consumer;
+    private final String topic;
+    private ExecutorService executor;
     protected static org.hbase.async.HBaseClient client;
-    protected static TSDB tsdb;
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainClass.class);
-    private static byte[] key;
-    private static final byte[] family = "d".getBytes();
-    private static final Calendar CalendarObj = Calendar.getInstance();
-    private static int a_numThreads;
-    private static ExecutorService executor;
+    private static TSDB tsdb;   
+    static final Logger log = LoggerFactory.getLogger(MainClass.class);
+
+    public MainClass(String a_zookeeper, String a_groupId, String a_topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+                createConsumerConfig(a_zookeeper, a_groupId));
+        this.topic = a_topic;
+    }
+
+    public void shutdown() {
+        if (consumer != null) {
+            consumer.shutdown();
+        }
+        if (executor != null) {
+            executor.shutdown();
+        }
+        try {
+            if (!executor.awaitTermination(5000, TimeUnit.MILLISECONDS)) {
+                System.out.println("Timed out waiting for consumer threads to shut down, exiting uncleanly");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during shutdown, exiting uncleanly");
+        }
+    }
+
+    public void run(int a_numThreads) {
+        Map<String, Integer> topicCountMap = new HashMap<>();
+        topicCountMap.put(topic, a_numThreads);
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+
+        // now launch all the threads
+        //
+        executor = Executors.newFixedThreadPool(a_numThreads);
+
+        // now create an object to consume the messages
+        //
+        int threadNumber = 0;
+        for (final KafkaStream stream : streams) {
+            executor.submit(new CalcByKafkaTask(stream, threadNumber, tsdb, client));
+            threadNumber++;
+        }
+    }
+
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", a_zookeeper);
+        props.put("group.id", a_groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
+
+        return new ConsumerConfig(props);
+    }
 
     public static void main(String[] args) throws Exception {
-
         String argskey = "";
-        String time = "1h-ago";
-        String end_time = "now";
-        Short daycount = 1;
-
+        int time = 300000;
+        int threads = 1;
         String configfile = "config.yaml";
         String logconfig = "log4j.properties";
 
-        Path path = Paths.get(configfile);
-
+        Path path = Paths.get(configfile);        
+        
         if (Files.notExists(path)) {
             configfile = "target/classes/config.yaml";
         }
@@ -82,68 +114,32 @@ public class MainClass {
         if (Files.notExists(path)) {
             logconfig = "target/classes/log4j.properties";
         }
-
+        
         for (String s : args) {
-            if (argskey.equals("-st")) {
-                time = s;
+            if (argskey.equals("-t")) {
+                time = Integer.parseInt(s);
             }
-            if (argskey.equals("-et")) {
-                end_time = s;
+            if (argskey.equals("-p")) {
+                threads = Integer.parseInt(s);
             }
             if (argskey.equals("-l")) {
                 logconfig = s;
             }
             if (argskey.equals("-c")) {
                 configfile = s;
-            }
-            if (argskey.equals("-d")) {
-                daycount = Short.parseShort(s);
-            }
+            }            
             argskey = s;
         }
-
-        path = Paths.get(configfile);
-
-        if (Files.notExists(path)) {
-            throw new FileNotFoundException(path.toString());
-        }
-
-        path = Paths.get(logconfig);
-
-        if (Files.notExists(path)) {
-            throw new FileNotFoundException(path.toString());
-        }
-
-        final Calendar EndCalendarObj = Calendar.getInstance();
-        final Calendar StartCalendarObj = Calendar.getInstance();
-        StartCalendarObj.setTime(new Date(DateTime.parseDateTimeString(time, null)));
-        EndCalendarObj.setTime(new Date(DateTime.parseDateTimeString(end_time, null)));
-        EndCalendarObj.set(Calendar.MILLISECOND, 0);
-        EndCalendarObj.set(Calendar.SECOND, 0);
-        EndCalendarObj.set(Calendar.MINUTE, 0);
-        EndCalendarObj.add(Calendar.MILLISECOND, -1);
-        StartCalendarObj.set(Calendar.MILLISECOND, 0);
-        StartCalendarObj.set(Calendar.SECOND, 0);
-        StartCalendarObj.set(Calendar.MINUTE, 0);
 
         PropertyConfigurator.configure(logconfig);
         Yaml yaml = new Yaml();
         Map<String, Object> conf = (Map<String, Object>) yaml.load(new InputStreamReader(new FileInputStream(configfile)));
-
-        if (EndCalendarObj.getTimeInMillis() < StartCalendarObj.getTimeInMillis()) {
-            throw new Exception("End time " + EndCalendarObj.getTime() + " must be greater than the start time " + StartCalendarObj.getTime());
-        }
-
-        LOGGER.warn("Start calculate From " + StartCalendarObj.getTime() + " to " + EndCalendarObj.getTime());
-        String current = new java.io.File(".").getCanonicalPath();
-        LOGGER.debug("Current dir:" + current);
+        
 
         String quorum = String.valueOf(conf.get("zkHosts"));
-        client = new org.hbase.async.HBaseClient(quorum);
-
-        String metatable = String.valueOf(conf.get("metatable"));
-
-        net.opentsdb.utils.Config openTsdbConfig = new net.opentsdb.utils.Config(true);
+        client = new org.hbase.async.HBaseClient(quorum);        
+        
+        Config openTsdbConfig = new net.opentsdb.utils.Config(true);
         openTsdbConfig.overrideConfig("tsd.core.auto_create_metrics", String.valueOf(conf.get("tsd.core.auto_create_metrics")));
         openTsdbConfig.overrideConfig("tsd.storage.enable_compaction", String.valueOf(conf.get("tsd.storage.enable_compaction")));
         openTsdbConfig.overrideConfig("tsd.storage.hbase.data_table", String.valueOf(conf.get("tsd.storage.hbase.data_table")));
@@ -152,57 +148,26 @@ public class MainClass {
                 client,
                 openTsdbConfig);
 
-//        OddeeyMetricMeta mtrsc;
-        OddeeyMetricMetaList mtrscList;
-        try {
-            mtrscList = new OddeeyMetricMetaList(tsdb, metatable.getBytes());
-        } catch (Exception ex) {
-            mtrscList = new OddeeyMetricMetaList();
-        }
+        String zooKeeper = String.valueOf(conf.get("kafka.zkHosts"));
+        String groupId = String.valueOf(conf.get("kafka.groupId"));
+        String topic = String.valueOf(conf.get("kafka.topic"));
 
-//        final Map<String, String> list = new HashMap<>();
-//        final Map<String, String> cluster = new HashMap<>();
-//        final Map<String, String> host = new HashMap<>();
-//        for (OddeeyMetricMeta mtrsc : mtrscList.values()) {
-//            list.put(mtrsc.getTags().get("UUID").getValue(), mtrsc.getTags().get("UUID").getValue());
-//            cluster.put(mtrsc.getTags().get("cluster").getValue(), mtrsc.getTags().get("cluster").getValue());
-//            host.put(mtrsc.getTags().get("host").getValue(), mtrsc.getTags().get("host").getValue());
-//
-//        }
-//        System.out.println(list.size());
-//        System.out.println(cluster.size());
-//        System.out.println(cluster);
-//        System.out.println(host.size());
-        int i = 0;
-        long Allstarttime = System.currentTimeMillis();
-        int threadNumber = 0;
-        try {
-            executor = Executors.newFixedThreadPool(1);
+        final Calendar CalendarObj = Calendar.getInstance();
 
-            // now create an object to consume the messages
-            //
-            for (OddeeyMetricMeta mtrsc : mtrscList.values()) {
+        MainClass example = new MainClass(zooKeeper, groupId, topic);
+        example.run(threads);
+        if (time == -1) {
+            while (true) {
 
-                executor.submit(new CalculateRuleTask(threadNumber, mtrsc, StartCalendarObj, EndCalendarObj, metatable));
-                threadNumber++;
-//            i++;
             }
+        } else {
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException ie) {
 
-            executor.shutdown();
-//            final boolean done = executor.awaitTermination(1, TimeUnit.MINUTES);
-            while (!executor.awaitTermination(10, TimeUnit.MINUTES)) {
-                Thread.sleep(1);
             }
-        } catch (Exception e) {
-            throw new Exception(e);
+            example.shutdown();
         }
-
-        long Allendtime = System.currentTimeMillis() - Allstarttime;
-        LOGGER.warn(threadNumber + " of " + mtrscList.size() + " done in " + Allendtime / 1000 + " s");
-        client.flush();
-        LOGGER.warn("Flush all");
-        LOGGER.warn("mtrscList.size:" + mtrscList.size());
-
         System.exit(0);
     }
 
